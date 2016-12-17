4cad08d1fa070af57850865b463d0a8ae4bb1b8c
dcronkite <dcronkite@gmail.com>
2016-12-16 23:07:37 -0800

    Added server configuration json file.

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable }     from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Http, Response, Headers, RequestOptions, Request} from '@angular/http';
 import {Category, SearchResult, Item} from "./categories";
 import {Observable} from "rxjs";
 import 'rxjs/add/operator/map';
@@ -7,30 +7,38 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CategoryService {
+
+  public serverAddress: string = "http://localhost:8090";
+  private headers = new Headers({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Origin': 'localhost'
+  });
+  private options = new RequestOptions({headers: this.headers});
+
   constructor(private http: Http) {
+    this.http.get('../../server.json')
+      .map((r: Response) => this.serverAddress = r.json().data.address);
+    console.warn(this.serverAddress);
+    if (this.serverAddress[this.serverAddress.length - 1] == '/') {
+      this.serverAddress = this.serverAddress.substr(0, this.serverAddress.length-1);
+    }
   }
 
   search(term: string): Observable<SearchResult[]> {
-    let headers = new Headers({
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    });
-    let options = new RequestOptions({headers: headers});
-
     return this.http
-      .get(`http://mpe015:8090/api/search?query=${term}`, options)
+      .get(`${this.serverAddress}/api/search?query=${term}`, this.options)
       .map((r: Response) => r.json().search as SearchResult[]);
   }
 
   getCategory(stype: string, elementId: number): Observable<Category> {
     return this.http
-      .get(`http://mpe015:8090/api/${stype}/add/${elementId}`)
+      .get(`${this.serverAddress}/api/${stype}/add/${elementId}`, this.options)
       .map((r: Response) => r.json() as Category);
   }
 
   filterItems(querystring: string): Observable<any> {
     return this.http
-      .get(`http://mpe015:8090/api/filter?${querystring}`);
+      .get(`${this.serverAddress}/api/filter?${querystring}`, this.options);
   }
 }
