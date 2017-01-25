8c098ac83480a2d5edc261c525e7e07e6102072d
dcronkite <dcronkite@gmail.com>
2017-01-24 17:34:47 -0800

    Added classes to breadcrumb to more easily add/remove components.

--- src/app/breadcrumb/breadcrumb.component.ts
+++ src/app/breadcrumb/breadcrumb.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {EventItem} from "../categories";
+import {Component, OnInit, Input, EventEmitter} from '@angular/core';
+import {EventItem, Value} from "../categories";
+import {MenuItem} from "primeng/components/common/api";
 
 @Component({
   selector: 'app-breadcrumb',
@@ -8,28 +9,40 @@ import {EventItem} from "../categories";
 })
 export class BreadcrumbComponent implements OnInit {
 
-  private items: any[];
+  private items: PrimaryMenuItem[];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    this.items = [{label: 'test'}];
+    this.items = [];
+    this.items.push(new OptionsMenuItem());
+  }
+
+  private getValue(item: EventItem) {
+    if (item.value) {
+      return item.value;
+    } else {
+      return `${item.values[0]}-${item.values[1]}`;
+    }
   }
 
   addItem(item: EventItem) {
-    var val;
-    if (item.value) {
-      val = item.value;
-    } else {
-      val = `${item.values[0]}-${item.values[1]}`;
-    }
-    this.items.push({
-      label: item.item,
-      items: [
-        {label: item.category},
-        {label: val}
-      ]
+    var val = this.getValue(item);
+    var itemToAdd = new PrimaryMenuItem(item.item, item.category, val);
+
+    var added: boolean = false;
+    this.items.forEach(mi => {
+      if (mi.label == item.item) {
+        // add value to item list
+        mi.addValue(val);
+        added = true;
+      }
     });
+
+    if (!added) {
+      this.items.push(itemToAdd);
+    }
   }
 
   removeItem(item: EventItem) {
@@ -37,3 +50,105 @@ export class BreadcrumbComponent implements OnInit {
   }
 
 }
+
+
+class OptionsMenuItem implements MenuItem {
+  label?: string;
+  icon?: string;
+  command?: (event?: any) => void;
+  url?: string;
+  routerLink?: any;
+  eventEmitter?: EventEmitter<any>;
+  items?: MenuItem[];
+  expanded?: boolean;
+  disabled?: boolean;
+
+  constructor() {
+    this.label = 'Options';
+    this.items = [
+      {label: 'Clear All Filters'},
+      {label: 'Export Filters'},
+    ]
+  }
+
+  addValue(val) {
+    this.items.push(new ValueMenuItem(val));
+  }
+}
+
+class PrimaryMenuItem implements MenuItem {
+  label?: string;
+  icon?: string;
+  command?: (event?: any) => void;
+  url?: string;
+  routerLink?: any;
+  eventEmitter?: EventEmitter<any>;
+  items?: MenuItem[];
+  expanded?: boolean;
+  disabled?: boolean;
+
+  constructor(label: string, category: string = null, value: string = null) {
+    this.label = label;
+    if (value && category) {
+      this.items = [
+        new CategoryMenuItem(category),
+        new ValueMenuItem(value)
+      ];
+    }
+  }
+
+  addValue(val) {
+    this.items.push(new ValueMenuItem(val));
+  }
+}
+
+class ValueMenuItem implements MenuItem {
+  label?: string;
+  icon?: string;
+  command?: (event?: any) => void;
+  url?: string;
+  routerLink?: any;
+  eventEmitter?: EventEmitter<any>;
+  items?: MenuItem[];
+  expanded?: boolean;
+  disabled?: boolean;
+
+  constructor(label: string) {
+    this.label = label;
+    this.items = [new RemoveMenuItem()];
+  }
+}
+
+class RemoveMenuItem implements MenuItem {
+  label?: string;
+  icon?: string;
+  command?: (event?: any) => void;
+  url?: string;
+  routerLink?: any;
+  eventEmitter?: EventEmitter<any>;
+  items?: MenuItem[];
+  expanded?: boolean;
+  disabled?: boolean;
+
+  constructor() {
+    this.label = 'Remove';
+  }
+}
+
+class CategoryMenuItem implements MenuItem {
+  label?: string;
+  icon?: string;
+  command?: (event?: any) => void;
+  url?: string;
+  routerLink?: any;
+  eventEmitter?: EventEmitter<any>;
+  items?: MenuItem[];
+  expanded?: boolean;
+  disabled?: boolean;
+
+  constructor(category: string) {
+    this.label = category;
+  }
+}
+
+
