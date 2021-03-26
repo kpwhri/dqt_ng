b8ea7eba03daef113187c77401d8d32dc763718a
David Cronkite <dcronkite@gmail.com>
2021-03-26 23:21:01 +0000

    Server address must be https

--- src/ENVIRONMENTS/environment.prod.ts
+++ src/ENVIRONMENTS/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   domain: 'act.kpwashingtonresearch.org',
-  serverAddress: 'http://act.kpwashingtonresearch.org:81'
+  serverAddress: 'https://act.kpwashingtonresearch.org:81'
 };
